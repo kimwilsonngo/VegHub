cf4300025a3649c31ea5d5f346fc7b2eb6bf4af9
Holly <kimwilsonngo@gmail.com>
2019-04-16 15:55:30 -0700

    Updated MyScreens and etc
    Fixed issue of not running.
    Added in Super Grid Gallery

--- src/MyScreens.js
+++ src/MyScreens.js
@@ -1,74 +1,27 @@
 import React, { Component } from 'react';
 
 //Weather module
-import { FlatList } from 'react-native';
-import ForecastCard from './components/ForecastCard';
-import { API_KEY } from './utils/WeatherAPIKey';
+import {  StyleSheet, View, Text, FlatList, Dimensions } from 'react-native';
+//import ForecastCard from './components/ForecastCard';
+//import { API_KEY } from './utils/WeatherAPIKey';
 
-import { Text, View, Dimensions } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const DeviceWidth = Dimensions.get('window').width
 const scaleVal = 0.4
 
 export class BagScreen extends Component {
-  constructor(props){
-  		super(props);
-
-  		this.state = {
-  			latitude: 0,
-  			longitude: 0,
-        temperature: 0,
-        weatherCondition: null,
-  			forecast: [],
-  			error:''
-  		};
-  	}
-
-  	componentDidMount(){
-  		// Get the user's location
-  		this.getLocation();
-  	}
-
-  	getLocation(){
-
-  		// Get the current position of the user
-  		navigator.geolocation.getCurrentPosition(
-  			(position) => {
-  				this.setState(
-  					(prevState) => ({
-  					latitude: position.coords.latitude,
-  					longitude: position.coords.longitude
-  					}), () => { this.getWeather(); }
-  				);
-  			},
-  			(error) => this.setState({ forecast: error.message }),
-  			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-  		);
-  	}
-
-  	getWeather(){
-
-  		// Construct the API url to call
-  		let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + `&units=imperial&appid=${API_KEY}`;
-
-
-  		// Call the API, and set the state of the weather forecast
-  		fetch(url)
-  		.then(response => response.json())
-  		.then(data => {
-  			this.setState((prevState, props) => ({
-  				forecast: data
-  			}));
-  		})
-  	}
-
-  	render() {
-  		return (
-  			<FlatList data={this.state.forecast.list} style={{marginTop:20}} keyExtractor={item => item.dt_txt} renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
-  		);
-  	}
+  render() {
+      return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Hello world BAG SCREEN </Text>
+        </View>
+      );
+    }
 }
+
 
 export class HomeScreen extends Component {
   render() {
@@ -130,13 +83,39 @@ export class PlantsResultsScreen extends Component {
 }
 
 export class PlantsScreen extends Component {
+
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello world PLANTS</Text>
-      </View>
-    );
-  }
+      const items = [
+        { name: 'TURQUOISE', code: '#1abc9c' }, { name: 'EMERALD', code: '#2ecc71' },
+        { name: 'PETER RIVER', code: '#3498db' }, { name: 'AMETHYST', code: '#9b59b6' },
+        { name: 'WET ASPHALT', code: '#34495e' }, { name: 'GREEN SEA', code: '#16a085' },
+        { name: 'NEPHRITIS', code: '#27ae60' }, { name: 'BELIZE HOLE', code: '#2980b9' },
+        { name: 'WISTERIA', code: '#8e44ad' }, { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
+        { name: 'SUN FLOWER', code: '#f1c40f' }, { name: 'CARROT', code: '#e67e22' },
+        { name: 'ALIZARIN', code: '#e74c3c' }, { name: 'CLOUDS', code: '#ecf0f1' },
+        { name: 'CONCRETE', code: '#95a5a6' }, { name: 'ORANGE', code: '#f39c12' },
+        { name: 'PUMPKIN', code: '#d35400' }, { name: 'POMEGRANATE', code: '#c0392b' },
+        { name: 'SILVER', code: '#bdc3c7' }, { name: 'ASBESTOS', code: '#7f8c8d' },
+      ];
+
+      return (
+        <FlatGrid
+          itemDimension={130}
+          items={items}
+          style={styles.gridView}
+          // staticDimension={300}
+          // fixed
+          // spacing={20}
+          renderItem={({ item, index }) => (
+            <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={styles.itemCode}>{item.code}</Text>
+            </View>
+          )}
+        />
+      );
+    }
+
 }
 
 export class CameraScreen extends Component {
@@ -148,3 +127,27 @@ export class CameraScreen extends Component {
         );
       }
 }
+
+
+const styles = StyleSheet.create({
+  gridView: {
+    marginTop: 20,
+    flex: 1,
+  },
+  itemContainer: {
+    justifyContent: 'flex-end',
+    borderRadius: 5,
+    padding: 10,
+    height: 150,
+  },
+  itemName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
+  },
+});
